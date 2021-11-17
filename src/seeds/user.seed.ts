6411837d0bcd1ec1d2202c5221d4e0fff9205b29
zulhusni hariz <zulhusnihariz97@gmail.com>
2021-11-18 02:33:41 +0800

    update user seed

--- src/seeds/user.seed.ts
+++ src/seeds/user.seed.ts
@@ -16,6 +16,13 @@ export default class CreateUsers implements Seeder {
           mobile: '013-1231251',
           roleId: '06d830c6-85d4-4bef-8d2d-197b3ceb3b93',
         },
+        {
+          id: '341d94ef-1e8a-4f2c-992d-956e23004909',
+          username: 'Molly',
+          email: 'molly@gmail.com',
+          mobile: '013-3216543',
+          roleId: '3aee61eb-6a83-4a72-b1aa-311323710c3b',
+        },
       ])
       .execute()
 
@@ -26,6 +33,10 @@ export default class CreateUsers implements Seeder {
       .values([
         {
           userId: '7099e081-2261-4725-8a9c-336952a939e2',
+          password: '1234',
+        },
+        {
+          userId: '341d94ef-1e8a-4f2c-992d-956e23004909',
           password: '1234',
         },
       ])
