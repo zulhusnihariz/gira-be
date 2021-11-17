739c09292a9c2c117db53930543f2841527c6aa5
zulhusni hariz <zulhusnihariz97@gmail.com>
2021-11-17 17:37:46 +0800

    add user-role relation

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -6,9 +6,10 @@ import {
   CreateDateColumn,
   PrimaryGeneratedColumn,
   JoinColumn,
+  ManyToOne,
 } from 'typeorm'
 import { Credential } from './credential.entity'
-
+import { Role } from './role.entity'
 @Entity()
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn('uuid')
@@ -23,9 +24,15 @@ export class User extends BaseEntity {
   @Column()
   mobile: string
 
+  @Column({ nullable: true })
+  roleId: string
+
   //TODO: setup cascade delete for user-credential
   @OneToOne(() => Credential)
   credential: Credential
+
+  @ManyToOne(() => Role, role => role.user, { eager: true })
+  role: Role
 
   @CreateDateColumn({ type: 'timestamp' })
   createdAt: Date
